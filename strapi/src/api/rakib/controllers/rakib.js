e0f6873ad6dcbbae4831078bb7aec08febd95e50
starttools <132042762+starttools@users.noreply.github.com>
2024-01-06 11:37:48 +0000

    fff

---
+++ strapi/src/api/rakib/controllers/rakib.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rakib controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::rakib.rakib');
---
+++ strapi/src/api/rakib/services/rakib.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rakib service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::rakib.rakib');
